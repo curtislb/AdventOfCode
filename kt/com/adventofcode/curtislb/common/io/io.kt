f8265073fe3a0f151013e403dd17f0f0209828d0
Curtis Belmonte <curtis.belmonte@gmail.com>
2019-12-09 00:41:16 -0800

    Add documentation for input path function

--- kt/com/adventofcode/curtislb/common/io/io.kt
+++ kt/com/adventofcode/curtislb/common/io/io.kt
@@ -4,7 +4,10 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 /**
- * TODO
+ * Produces the path corresponding to the input file for a given puzzle.
+ * @param day The integer day corresponding to that of the puzzle.
+ * @param fileName The name of the input file, with the path omitted.
+ * @return A [Path] object representing the location of the input file on the current file system.
  */
 fun pathToInput(day: Int, fileName: String): Path {
     return Paths.get("input", "day${day.toString().padStart(2, '0')}", fileName)
